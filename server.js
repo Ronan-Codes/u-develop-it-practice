02e35d3cbf223d4fa052683511f67b43f5cc0047
Ronan-Codes <galv.ronan@gmail.com>
2021-05-04 14:29:50 -0400

    added db.query calls. Next incorporate it into API calls

--- server.js
+++ server.js
@@ -24,9 +24,41 @@ const db = mysql.createConnection(
 // replace the values of user and password with your MySQL username and password.
 
 // make SQL query to database. Returns rows matching query
-db.query(`SELECT * FROM candidates`, (err, rows) => {
-    console.log(rows);
-})
+// db.query(`SELECT * FROM candidates`, (err, rows) => {
+//     console.log(rows);
+// })
+
+// GET a single candidate 
+// db.query(`Select * FROM candidates WHERE id = 1`, (err, row) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(row);
+// })
+
+// Delete a candidate 
+    // ? placeholder makes this a `prepared statement` 
+    // 1 is a hardcoded param. Param argument can be array.
+    // result shows which one got deleted?
+// db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(result)
+// })
+
+// Create a candidate
+    // Not setting id will create new id automatically 
+// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+//                 Values (?,?,?,?)`;
+// const params = [1, 'Ronald', 'Firbank', 1];
+
+// db.query(sql, params, (err, result) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(result);
+// });
 
 // Default response for any other request (NOT FOUND) 
     // Make sure this is the last route
