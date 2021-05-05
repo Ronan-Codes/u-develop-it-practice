07325711b05d172a373136c47c78f5aa0170808a
Ronan-Codes <galv.ronan@gmail.com>
2021-05-05 17:08:04 -0400

    Added JOIN statements for parties on candidates table for GET routes.

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,8 @@
+DROP TABLE IF EXISTS candidates;
+DROP TABLE IF EXISTS parties;
+-- candidates table must be dropped before the parties table due to 
+-- the foreign key constraint that requires the parties table to exist.
+
 CREATE TABLE parties (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(50) NOT NULL,
@@ -8,5 +13,22 @@ CREATE TABLE candidates (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
-  industry_connected BOOLEAN NOT NULL
+  party_id INTEGER,
+  industry_connected BOOLEAN NOT NULL,
+  CONSTRAINT fk_party FOREIGN KEY (party_id) References parties(id) ON DELETE SET NULL
 );
+-- party_id field is official foreign key. References parties(id).
+-- B/c the constraint relies on parties table, parties is defined 1st.
+-- ON DELETE of parties(id) set party_id to NULL
+
+-- A foreign key is a field in one table that references the primary key of another table. 
+-- In this case, Ronald Firbank's row in candidates would include a 
+-- field with the number 1, but we'd know that that refers to a row 
+-- in the parties table with the same id value. Take a look at 
+-- the following image to better understand the relationship:
+
+-- ATER TABLE: 
+-- add new field, delete field, modify field 
+  -- 1. ALTER TABLE candidates ADD COLUMN party_id INTEGER;
+    -- Preps candidate table with foreign key
+  -- 2. DROP TABLE IF EXISTS candidates;
--- server.js
+++ server.js
@@ -27,7 +27,11 @@ const inputCheck = require('./utils/inputCheck');
 
 // Get all candidates 
 app.get('/api/candidates', (req, res) => {
-    const sql = `SELECT * FROM candidates`;
+    const sql = `SELECT candidates.*, parties.name
+                AS party_name
+                FROM candidates
+                LEFT JOIN parties
+                ON candidates.party_id = parties.id`;
 
     db.query(sql, (err, rows) => {
         if(err) {
@@ -45,7 +49,12 @@ app.get('/api/candidates', (req, res) => {
 // ParamsNote: can change params :id into anythin. Ex: age.
     // Format: /api/candidates/7 
 app.get('/api/candidate/:id', (req, res) => {
-    const sql = `SELECT * FROM candidates WHERE id = ?`;
+    const sql = `SELECT candidates.*, parties.name
+                AS party_name
+                FROM candidates
+                LEFT JOIN parties
+                ON candidates.party_id = parties.id
+                WHERE candidates.id = ?`;
     const params = [req.params.id];
 
     db.query(sql, params, (err, row) => {
